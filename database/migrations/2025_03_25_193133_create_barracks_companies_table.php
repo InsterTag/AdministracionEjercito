a7a0e6ec90784197598b90084e35e5771c49d216
InsterTag <cajassandovalestiven01@gmail.com>
2025-03-25 17:19:51 -0500

    Se arregla

--- database/migrations/2025_03_25_193133_create_barracks_companies_table.php
+++ database/migrations/2025_03_25_193133_create_barracks_companies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('barracks_companies', function (Blueprint $table) {
             $table->id();
 
+            $table->string('date');
             $table->unsignedBigInteger('barracks_fk')->nullable();
             $table->foreign('barracks_fk')
             ->references('id')
